a72bde41956047493f2c8db9cf2cd8b032d5310a
Sudhanshu kumar <sudhanshu@euron.one>
2026-09-20 09:20:00 +0530

    test

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
 print("Hello, World!")
 print("Hello again!")
 print("SUDH1 branch.")
-<<<<<<< HEAD
-print("This is my developer 1. Changes made by developer 1.")
-=======
 print("This is our dev 3, and this is our dev 3 change.")
->>>>>>> f7eade2 ( this is a dev 3 :)
